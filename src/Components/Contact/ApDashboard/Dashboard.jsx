03bd1377a7e3ad13bc124a83e84cba582dadf24a
muhaiminultasin <tasinmuhaiminul@gmail.com>
2024-11-06 23:18:32 +0600

    dashboard

--- src/Components/Contact/ApDashboard/Dashboard.jsx
+++ src/Components/Contact/ApDashboard/Dashboard.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import Title from '../../common/title'
+import Step from '../../common/Step'
+import Footer from '../../common/Footer'
 
 function Dashboard() {
+
+    const steps = [
+        
+        {
+            img:"PBImage/apply.png",
+            title:"Your Application",
+            subtitle: "Complete the form by providing all necessary information and as many details as possible."
+        },
+        {
+            img:"PBImage/review.png",
+            title:"Team Review",
+            subtitle: "Our team will carefully review the details of your application, assessing all the information provided to ensure it meets our criteria and requirements."
+        },
+        {
+            img:"PBImage/decision.png",
+            title:"Our Decision",
+            subtitle: "If approved, you'll receive an email with instructions to access your dashboard."
+        },
+    ]
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className='py-[50px] px-[20px] md:px-[70px] lg:px-[140px]'>
+      <div>
+      <Title titleClass={"text-[25px] md:text-[35px] lg:text-[40px] md:text-center"} title={"Dashboard Application"} />
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:flex flex-wrap'>
+            {
+                steps.map( ({img,title,subtitle},i)=> {
+                        return <Step className={"mx-auto mt-10  md:w-[300px]"} key={i} img={img} title={title} subtitle={subtitle} />
+                })
+            }
+        </div>
+
+         <iframe src="https://tally.so/r/mYz7xJ" frameborder="0" className='w-full h-[100vh]'></iframe>
+      </div>
+            <Footer/>
+
     </div>
   )
 }
--- src/Components/Contact/Ifram/Iframe.jsx
+++ src/Components/Contact/Ifram/Iframe.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Footer from '../../common/Footer'
 
 function Iframe() {
   return (
     <div>
       <iframe src="https://tally.so/r/nGBO5Z" frameborder="0" className='w-full h-screen'></iframe>
+      <Footer/>
     </div>
+
+   
   )
 }
 
