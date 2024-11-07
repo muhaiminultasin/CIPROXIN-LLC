de975a098c6178bf17703087cf6b5133613f4365
muhaiminultasin <tasinmuhaiminul@gmail.com>
2024-11-07 07:41:35 +0600

    rederect path

--- src/Components/Home/HomeCompomemts/SingUpBlock.jsx
+++ src/Components/Home/HomeCompomemts/SingUpBlock.jsx
@@ -5,7 +5,7 @@ function SingUpBlock() {
   return (
     <section className='bg-[url(/PBImage/equipment.jpg)] bg-center  bg-no-repeat px-[20px] md:px-[70px] lg:px-[140px] py-[100px] opacity-100'>
       <div>
-        <Title titleClass={"text-[25px] md:text-[40px] text-white md:text-center max-w-[700px"} subtitleClass={"text-white"} title={"JFueling a Global Community of Creators and Fans — Let’s Grow Together with Ciproxin LLC"} subtitle={"Ciproxin LLC is a premier music distribution platform helping artists reach audiences worldwide on Spotify, Apple Music, TikTok, and more. Contact us today and start sharing your music with millions!"}/>
+        <Title titleClass={"text-[25px] md:text-[40px] text-white md:text-center max-w-[700px"} subtitleClass={"text-white"} title={"Start selling your music Worldwide today"} subtitle={"Ciproxin LLC is a premier music distribution platform helping artists reach audiences worldwide on Spotify, Apple Music, TikTok, and more. Contact us today and start sharing your music with millions!"}/>
       </div>
 
       {/* <div className="flex flex-col md:flex-row md:gap-5 justify-center ">
